4376f6a2ce9885d7cbc59a06e730868a96f83075
Nils-johansson-nti-johanneberg <nils.johansson2@elev.ga.ntig.se>
2020-03-28 23:01:29 +0100

    Edit
    Bad media query
    Js implemented

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,37 @@
-$(selector).hover(function() {
-    $(this).addClass(earthBox);
-    }, function() {
-  $(this).removeClass(earthBox);
-  });
+document.getElementById("earth").addEventListener("mouseover", mouseOver);
+document.getElementById("earth").addEventListener("mouseout", mouseOut);
+
+function mouseOver(){
+  this.classList.add("earthBox");
+}
+function mouseOut(){
+  this.classList.remove("earthBox");
+}
+document.getElementById("mars").addEventListener("mouseover", mouseOver);
+document.getElementById("mars").addEventListener("mouseout", mouseOut);
+
+document.getElementById("jupiter").addEventListener("mouseover", mouseOver);
+document.getElementById("jupiter").addEventListener("mouseout", mouseOut);
+
+document.getElementById("sun").addEventListener("mouseover", mouseOver);
+document.getElementById("sun").addEventListener("mouseout", mouseOut);
+
+document.getElementById("venus").addEventListener("mouseover", mouseOver);
+document.getElementById("venus").addEventListener("mouseout", mouseOut);
+
+document.getElementById("saturnus").addEventListener("mouseover", mouseOver);
+document.getElementById("saturnus").addEventListener("mouseout", mouseOut);
+
+document.getElementById("merkurius").addEventListener("mouseover", mouseOver);
+document.getElementById("merkurius").addEventListener("mouseout", mouseOut);
+
+document.getElementById("uranus").addEventListener("mouseover", mouseOver);
+document.getElementById("uranus").addEventListener("mouseout", mouseOut);
+
+document.getElementById("neptunus").addEventListener("mouseover", mouseOver);
+document.getElementById("neptunus").addEventListener("mouseout", mouseOut);
+
+document.getElementById("pluto").addEventListener("mouseover", mouseOver);
+document.getElementById("pluto").addEventListener("mouseout", mouseOut);
+
+
